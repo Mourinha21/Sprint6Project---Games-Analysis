d7d756f27a17f580b0877864df3fe51564042e90
Mourinha <rickgomes5844@gmail.com>
2026-05-17 14:10:27 -0300

    Corrections and adding a few graphics

--- app.py
+++ app.py
@@ -24,11 +24,26 @@ games['rating'] = games['rating'].fillna('???')
 
 games.drop(games[games['year_of_release'] == 1000].index, axis=0, inplace=True)
 
-games['critic_score'] = games['critic_score'].fillna(69.0)
+mean_genres = games.groupby('genre')['critic_score'].mean().to_dict()
+
+games['critic_score'] = games.apply(
+    lambda row: mean_genres[row['genre']]
+    if pd.isna(row['critic_score'])
+    else row['critic_score'],
+    axis=1
+)
 
 games['user_score'] = games['user_score'].replace('tbd', '6.9')
 games['user_score'] = pd.to_numeric(games['user_score'])
-games['user_score'] = games['user_score'].fillna(6.9)
+
+mean_genres = games.groupby('genre')['user_score'].mean().to_dict()
+
+games['user_score'] = games.apply(
+    lambda row: mean_genres[row['genre']]
+    if pd.isna(row['user_score'])
+    else row['user_score'],
+    axis=1
+)
 
 games.drop_duplicates(inplace=True)
 games = games.reset_index(drop=True)
@@ -56,7 +71,7 @@ ga_t.pop(0)
 games_ano = ga_t.T
 games_ano.sort_values('year_of_release')
 
-plt.figure(figsize=(21, 9))
+plt.figure(figsize=(16, 9))
 plt.bar(games_ano['year_of_release'],
         games_ano['number_of_games'], picker=True)
 plt.xlabel('Year of Release')
@@ -70,7 +85,7 @@ sales_plat_sorted = sales_plat.sort_values(
     by=['platform', 'year_of_release']
 )
 
-plt.figure(figsize=(21, 9))
+plt.figure(figsize=(16, 9))
 for platform, df in sales_plat_sorted.groupby('platform'):
     plt.plot(
         df['year_of_release'],
@@ -90,13 +105,12 @@ first_decade_sales = sales_plat.query(
 first_decade_sales.sort_values(
     by=['year_of_release', 'total_sales'], inplace=True)
 
-plt.figure(figsize=(21, 9))
-plt.scatter(x=first_decade_sales['year_of_release'],
-            y=first_decade_sales['platform'], s=first_decade_sales['total_sales'])
-plt.xlabel('Year')
-plt.ylabel('Platform')
-plt.title('Sales of Games By Platform For Each Year')
-plt.show()
+f, ax = plt.subplots(figsize=(9,6))
+
+
+plt.title('Total Sales by Year and Platform - Millions')
+ax.set(xlabel='Year of Release', ylabel='Platform')
+sns.heatmap(first_decade_sales, annot=True, fmt='.1f', linewidths=.5, ax=ax)
 
 top_plat = games[(games['platform'] == 'Wii') | (games['platform'] == 'DS') | (games['platform'] == 'PS3')
                  | (games['platform'] == 'PS2') | (games['platform'] == 'X360')]
@@ -107,7 +121,6 @@ sns.boxplot(data=top_plat, x='platform',
 plt.xlabel('Plataforms')
 plt.ylabel('Average Sales - Millions')
 plt.title('Average Sales on main Platforms')
-# Aplicando escala logaritmica para visualização do boxplot e convertendo os ticks do eixo y para melhor visualização.
 plt.yscale('log')
 ticks = [0.01, 0.1, 1, 10, 100]
 plt.yticks(ticks, [str(t) for t in ticks])
@@ -116,12 +129,12 @@ plt.show()
 media_plat = games.groupby('platform')['total_sales'].mean(
 ).reset_index().sort_values(by='total_sales', ascending=False)
 
-plt.figure(figsize=(21, 9))
+plt.figure(figsize=(12, 6))
 plt.scatter(data=games, x='user_score', y='total_sales')
 plt.title(
-    'Total Sales Distribution By Users Score', fontsize=16)
+    'Total Sales Distribution By User Scores', fontsize=16)
 plt.ylabel('Global Sales (in millions)', fontsize=12)
-plt.xlabel('Users Score', fontsize=12)
+plt.xlabel('User Score', fontsize=12)
 plt.tight_layout()
 plt.show()
 
@@ -129,7 +142,7 @@ re2_sales = games.query('name == "Resident Evil 2"')
 
 plt.figure(figsize=(16, 9))
 plt.bar(x='platform', height='total_sales', data=re2_sales)
-plt.title('Resident Evil 2 Total Sales Distribution By Platform', fontsize=16)
+plt.title('Comparison Between Platforms on Resident Evil 2  Sales', fontsize=16)
 plt.ylabel('Total Sales (in millions)', fontsize=12)
 plt.xlabel('Platform', fontsize=12)
 plt.show()
@@ -167,12 +180,32 @@ pc_users_score = games.query('platform == "PC"')
 alpha = 0.05
 results = st.ttest_ind(
     xone_users_score['user_score'], pc_users_score['user_score'])
-print('valor-p: ', results.pvalue)
+print('p-value: ', results.pvalue)
 
-if results.pvalue < alpha:  # comparando o valor-p com o limiar
-    print("Rejeitamos a hipótese nula")
+if results.pvalue < alpha: 
+    print("We reject the null hypothesis")
 else:
-    print("Não podemos rejeitar a hipótese nula")
+    print("We can't reject the null hypothesis")
+
+print("The p-value here tells that there is a difference between users'"
+      " rating the same game on different platforms (XONE and PC)")
+
+consoles_means = [xone_users_score['user_score'].mean(), pc_users_score['user_score'].mean()]
+
+fig, ax = plt.subplots(figsize=(8,8))
+
+bars = ax.bar(
+    ['Xbox One', 'PC'],
+    consoles_means,
+    color=['green', 'blue']
+)
+
+ax.bar_label(bars, fmt='%.2f', padding=3)
+
+ax.set_title("Average User Scores for Xbox One and PC Games")
+ax.set_ylim(6.5, 7.10)
+
+plt.show()
 
 user_score_action = games.query('genre == "Action"')
 user_score_sports = games.query('genre == "Sports"')
@@ -180,9 +213,28 @@ user_score_sports = games.query('genre == "Sports"')
 alpha = 0.05
 results = st.ttest_ind(
     user_score_action['user_score'], user_score_sports['user_score'])
-print('valor-p: ', results.pvalue)
+print('p-value: ', results.pvalue)
 
-if results.pvalue < alpha:  # comparando o valor-p com o limiar
-    print("Rejeitamos a hipótese nula")
+if results.pvalue < alpha:
+    print("We reject the null hypothesis")
 else:
-    print("Não podemos rejeitar a hipótese nula")
+    print("We can't reject the null hypothesis")
+
+print('The p-value tells that')
+
+act_sports_means = [user_score_action['user_score'].mean(), user_score_sports['user_score'].mean()]
+
+fig, ax = plt.subplots(figsize=(8,8))
+
+bars = ax.bar(
+    ['Action', 'Sports'],
+    act_sports_means,
+    color=['orange', 'turquoise']
+)
+
+ax.bar_label(bars, fmt='%.2f', padding=3)
+
+ax.set_title("Average User Scores for Action and Sports Games")
+ax.set_ylim(6.85, 7.05)
+
+plt.show()
